36a04825b04e9bdcf44d10d65096a21674901855
Adam Oswalt <adam.oswalt@gmail.com>
2016-07-26 14:14:13 -0500

    code tweaks, docstrings, and README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Mathmagician
+
+A simple Python 3 CLI program that prints numbers based on user input.
+
+Run `python mathmagician.py` to begin the program.
+
+
+Example output:
+```
+Choose an option:
+1. Print Integers
+2. Print Fibonacci
+3. Print Primes
+4. Quit
+
+> fib
+Enter how many: 4
+0
+1
+1
+2
+```
--- mathmagician.py
+++ mathmagician.py
@@ -1,6 +1,7 @@
 import time
 
 def main():
+    '''Main execution of the program'''
     main_menu = {
         '1. Print Integers': print_integers,
         '2. Print Fibonacci': print_fibonacci,
@@ -18,17 +19,21 @@ def main():
 
 
 def prompt_menu(menu):
+    '''Accept a menu as a dictionary. Prompt user for part of a key'''
     choice = ""
     key_match = []
+    # if not only 1 result, keep prompting
     while len(key_match) != 1:
         print('\nChoose an option:')
         [print(entry) for entry in sorted(menu.keys())]
         print()
         choice = input('> ')
+        # get all keys for partial string match
         key_match = [key for key in menu.keys() if choice.lower() in key.lower()]
     return key_match[0]
 
 def prompt_amount():
+    '''Prompt for an integer value'''
     amount = -1
     while amount < 0:
         try:
@@ -39,10 +44,8 @@ def prompt_amount():
     return amount
 
 
-def slow_print(a_list): # pragma: no cover
-    '''
-    Print a list with some delay
-    '''
+def slow_print(a_list):
+    '''Print a list with some delay'''
 
     for n in a_list:
         print(n)
@@ -51,29 +54,31 @@ def slow_print(a_list): # pragma: no cover
 
 
 def print_integers(amount):
-    # offset by 1 to exclude 0
+    '''Print integers from 1 to amount'''
     nums = [i for i in range(amount + 1)[1:]]
     slow_print(nums)
 
 
 def fibs(amount):
+    '''Generator for an amount of Fibonacci numbers'''
     a, b = 0, 1
 
     yield a
     yield b
 
-    count = 2
-    while count < amount:
+    # starting after the first 2 numbers
+    for i in range(amount - 2):
         a, b = b, a + b
         yield b
-        count += 1
 
 def print_fibonacci(amount):
+    '''Print amount of Fibonacci numbers'''
     nums = [i for i in fibs(amount)]
     slow_print(nums)
 
 
 def is_prime(num):
+    '''Test if number is prime'''
     # All prime numbers, other than 2 & 3, are of the form 6n+/-1.
     # All composite numbers have prime factors
     if num == 2 or num == 3: return True
@@ -89,6 +94,7 @@ def is_prime(num):
     return True
 
 def primes(amount):
+    '''Generator for an amount of prime numbers'''
     count = 0
     num = 2
     while(count < amount):
@@ -98,6 +104,7 @@ def primes(amount):
         count += 1
 
 def print_primes(amount):
+    '''Print amount of prime numbers'''
     nums = [i for i in primes(amount)]
     slow_print(nums)
 
